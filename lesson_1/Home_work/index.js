c101d9f5fc2868222abcae1e7cc5209ef264b1f9
Nikita Popov <n.popov88@mail.ru>
2021-10-01 22:51:45 +0300

    Prettier ДЗ

--- lesson_1/Home_work/index.js
+++ lesson_1/Home_work/index.js
@@ -11,13 +11,25 @@ let whatHappensAfterdeath = null;
 let userName;
 
 const user3 = {
-    name: "Oleg",
-    surname: "Popov"
+  name: "Oleg",
+  surname: "Popov",
 };
 
-user3.name = "Natasha"; 
+user3.name = "Natasha";
 
 console.log(user3.name);
 
-console.log(myCurrentaAge, myName, userNameNikita,whatHappensAfterdeath,userName);
-console.log(typeof myCurrentaAge, typeof myName, typeof userNameNikita, typeof whatHappensAfterdeath, typeof userName);
+console.log(
+  myCurrentaAge,
+  myName,
+  userNameNikita,
+  whatHappensAfterdeath,
+  userName
+);
+console.log(
+  typeof myCurrentaAge,
+  typeof myName,
+  typeof userNameNikita,
+  typeof whatHappensAfterdeath,
+  typeof userName
+);
