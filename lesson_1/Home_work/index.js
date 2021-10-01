4f6291cdf0f560bb244c864a3844a0330a3546d3
Nikita Popov <n.popov88@mail.ru>
2021-10-01 22:42:28 +0300

    Оформление учебного рапозитория

---
+++ lesson_1/Home_work/index.js
@@ -0,0 +1,23 @@
+console.log("Hello world");
+
+const year = 2021;
+
+console.log(year);
+
+const myCurrentaAge = 33;
+const myName = "Nikita";
+let userNameNikita = true;
+let whatHappensAfterdeath = null;
+let userName;
+
+const user3 = {
+    name: "Oleg",
+    surname: "Popov"
+};
+
+user3.name = "Natasha"; 
+
+console.log(user3.name);
+
+console.log(myCurrentaAge, myName, userNameNikita,whatHappensAfterdeath,userName);
+console.log(typeof myCurrentaAge, typeof myName, typeof userNameNikita, typeof whatHappensAfterdeath, typeof userName);
---
+++ lesson_2/index.js
@@ -0,0 +1,1 @@
+console.log("Привет, я из NodeJS");
