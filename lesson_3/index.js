17476faa24e575c2f10c69f38f4261ce53f11d11
Nikita Popov <n.popov88@mail.ru>
2021-10-02 12:29:40 +0300

    Преобразование типов и операторы сравнения

---
+++ lesson_3/index.js
@@ -0,0 +1,83 @@
+// const firstValue = String(5);
+
+// console.log(typeof firstValue);
+
+// const obj = {
+//   foo: 67,
+// };
+
+// console.log(typeof String(obj.foo));
+// NUMBER
+
+// let testValue;
+
+// console.log(typeof testValue);
+
+// testValue = Number(testValue);
+
+// console.log(testValue);
+
+// NAN
+
+// const isUser = true;
+// const isAdmin = false;
+
+// console.log(Number(isUser), Number(isAdmin));
+
+// const voldString = "";
+
+// console.log(Number(voldString));
+// численные преобразования
+
+// BOOLEAN
+
+// const value = "0";
+
+// console.log(typeof Boolean(value), Boolean(value));
+
+// const obj = [].length;
+// console.log(Boolean(obj));
+
+// console.log(2 ** 2);
+
+// Унарный+
+
+// const value = "5";
+
+// console.log(typeof value, typeof +value, typeof Number(value));
+
+// Бинарный плюс
+
+// let sum = 5 + 6;
+// console.log(sum);
+
+// sum = 1 + true;
+// console.log(sum);
+
+// sum = 5 + null;
+// console.log(sum);
+
+// const stringValue = "Alex";
+// const numberValue = 34;
+
+// console.log(+stringValue + numberValue);
+// console.log(typeof (numberValue + +stringValue));
+
+// const firstName = "Alex";
+// const lastName = "Shirokov";
+
+// console.log(firstName + " " + lastName);
+
+// console.log(`${firstName} ${lastName}`);
+
+// Операции сравнения
+
+// console.log(5 == 5);
+
+// console.log(5 != 7);
+
+// console.log(false == 0);
+
+//
+
+console.log("\t\n" - 2);
