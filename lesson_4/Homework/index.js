56584dd181c1e59e75af3b5a447edf554cabc30a
Nikita Popov <n.popov88@mail.ru>
2021-10-15 23:42:26 +0300

    Домашняя работа к 16.10

---
+++ lesson_4/Homework/index.js
@@ -0,0 +1,161 @@
+// Работа с if-else
+// 1
+// let a = 0;
+
+// if (a == 0) {
+//   console.log("Верно");
+// } else {
+//   console.log("Неверно");
+// }
+
+// 2
+// let a = 0;
+
+// if (a > -3) {
+//   console.log("Верно");
+// } else {
+//   console.log("Неверно");
+// }
+
+// 3
+// let a = -1;
+
+// if (a < -3) {
+//   console.log("Верно");
+// } else {
+//   console.log("Неверно");
+// }
+
+// 4
+// let a = -15;
+
+// if (a >= 0) {
+//   console.log("Верно");
+// } else {
+//   console.log("Неверно");
+// }
+
+// 5
+// const a = 65;
+
+// if (a <= 0) {
+//   console.log("Верно");
+// } else {
+//   console.log("Неверно");
+// }
+
+// 6
+// const a = 0;
+// if (a != 0) {
+//   console.log("Верно");
+// } else {
+//   console.log("Неверно");
+// }
+
+// 7
+// let a = "тест";
+
+// if (a == "test") {
+//   console.log("Верно");
+// } else {
+//   console.log("Неверно");
+// }
+
+// 8
+// let a = "1";
+
+// if (a === "1") {
+//   console.log("Верно");
+// } else if (a == "1") {
+//   console.log("В принципе верно");
+// } else {
+//   console.log("Нeверно");
+// }
+
+// Работа с логическими переменными
+
+// const test = false;
+
+// if (test === true) {
+//   console.log("Верно");
+// } else {
+//   console.log("Неверно");
+// }
+
+// const test = false;
+// const test === true ? "Верно" : "Неверно";
+// console.log(test);
+// // // не получилось написать правильно короткую версию
+
+// let test = false;
+
+// if (test !== true) {
+//   console.log("Верно");
+// } else {
+//   console.log("Неверно");
+// }
+
+// Работа с && (и) и || (или)
+
+// let a = 2;
+// if (a > 0 && a < 5) {
+//   console.log("Верно");
+// } else {
+//   console.log("Неверно");
+// }
+
+// const a = 2;
+
+// if (a == 0 || a == 2) {
+//   result1 = a + 7;
+//   console.log(result1);
+// } else {
+//   result2 = a / 10;
+//   console.log(result2);
+// }
+
+// const a = 0;
+// const b = 6;
+
+// if (a <= 1 && b >= 3) {
+//   result1 = a + b;
+//   console.log(result1);
+// } else {
+//   result2 = a - b;
+//   console.log(result2);
+// }
+
+// const a = 25;
+// const b = 7;
+
+// if ((a > 2 && a < 11) || (b >= 6 && b < 14)) {
+//   console.log("Верно");
+// } else {
+//   console.log("Неверно");
+// }
+// вопрос: почему верно?
+
+// На switch-case
+
+// const num = 0;
+
+// switch (num) {
+//   case 1:
+//     result = "зима";
+//     console.log(result);
+//     break;
+//   case 2:
+//     result = "весна";
+//     console.log(result);
+//     break;
+//   case 3:
+//     result = "лето";
+//     console.log(result);
+//     break;
+//   case 4:
+//     result = "осень";
+//     console.log(result);
+//     break;
+//   default:
+//     console.log("Больше времен года нет");
+// }
