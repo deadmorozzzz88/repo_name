c40682cfc01f5f3063b91f3a947fc781b3b82421
Nikita Popov <n.popov88@mail.ru>
2021-10-16 13:30:11 +0300

    Учёба, 5 урок

---
+++ lesson_5/index.js
@@ -0,0 +1,194 @@
+// function helloWorld() {
+//   console.log("Hello world");
+// }
+
+// helloWorld();
+
+// const isUserAuth = true;
+// function checkAccess() {
+//   if (isUserAuth) {
+//     console.log("Доступ есть");
+//   } else {
+//     console.log("Доступа нет");
+//   }
+// }
+
+// checkAccess();
+
+// function writeMessage() {
+//   if (isUserAuth) {
+//     console.log("Привет Польователь");
+//   } else {
+//     console.log("пsривет гость");
+//   }
+// }
+// writeMessage();
+
+// function sum(a, b = 0) {
+//   console.log(b);
+//   console.log(a + b);
+// }
+// sum(1, 4);
+// sum(16);
+
+// function getName(name) {
+//   console.log("до return");
+//   return "name";
+//   console.log("после return");
+// }
+
+// const userName = getName("Max");
+// console.log(userName);
+
+// let name = "Alex";
+
+// function getName(userName) {
+//   return userName;
+// }
+
+// const nameUserOne = getName(name);
+// console.log(getName(nameUserOne));
+
+// name = "Ivan";
+
+// const nameUserTwo = getName(name);
+// console.log(getName(nameUserTwo));
+
+// let name = "Alex";
+
+// console.log(name, "1");
+
+// function sayHi(userName) {
+//   userName = "Petr";
+//   console.log(userName, "function");
+// }
+
+// sayHi(name);
+// console.log(name, "2");
+
+// let a = 0;
+// let b = 0;
+
+// function sum() {
+//   a += 1;
+//   b += 1;
+//   console.log(a + b);
+//   a + b;
+// }
+// sum();
+
+// function sum() {
+//   const a = 0;
+//   const b = 1;
+//   console.log(a + b);
+// }
+// sum();
+// sum();
+// sum();
+// sum();
+// sum();
+
+// const userName = "Nikita";
+
+// function writeMessage() {
+//   if ((userName = "Nikita")) {
+//     console.log("Hi Nikita");
+//   } else {
+//     console.log("Привет, кто бы то ни был");
+//   }
+// }
+
+// writeMessage();
+
+// let myName = "Nikita";
+
+// function userName(name) {
+//   if (name === "Nikita") {
+//     console.log(`Привет ${name}`);
+//   } else {
+//     console.log("Привет, кто бы то ни был");
+//   }
+// }
+
+// userName("Nikita");
+
+// function exp(a, b = 1) {
+//   console.log(a ** b);
+// }
+
+// exp(2, 2);
+// exp(2, 8);
+// exp(20);
+
+// const sayName = function (name) {
+//   return name;
+// };
+// const copy = sayName;
+// console.log(sayName("Alex"));
+// console.log(copy("Ivan"));
+
+// const getName = function () {
+//   console.log("Alex");
+// };
+// const getUserInfo = function (callback) {
+//   console.log("USER INFO");
+//   callback();
+// };
+
+// getUserInfo(getName);
+
+// function test() {
+//   console.log("test");
+// }
+// test();
+
+// const test2 = function () {
+//   console.log("test2");
+// };
+
+// test2();
+
+// const maFirstArrowFunc = () => console.log("!!");
+
+// const test1 = (name, age) => console.log(name, age);
+// test1("Alex", 23);
+
+// const test1 = (name) => {
+//   return name;
+// };
+// console.log(test1("Alex", 23));
+
+// const getLog = () => console.log("LOG");
+// getLog();
+
+// const callName = (name, count) => {
+//   for (let i = 0; i < count; i += 1) {
+//     console.log(name);
+//   }
+// };
+// callName("Nikita", 3);
+
+// callName("Oleg", 10);
+
+// let value = (letter) => {
+//   if (
+//     letter === "а" ||
+//     letter === "е" ||
+//     letter === "ё" ||
+//     letter === "и" ||
+//     letter === "о" ||
+//     letter === "у" ||
+//     letter === "ю" ||
+//     letter === "я"
+//   ) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// };
+
+// value("д");
+
+let myPolindrom = (word) => {
+ if ()
+}
