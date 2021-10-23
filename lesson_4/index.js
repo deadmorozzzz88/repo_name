2ed617cc87375b492c2728152c5226ff6c07559c
Nikita Popov <n.popov88@mail.ru>
2021-10-23 12:47:21 +0300

    Классная работа, урок 6

---
+++ lesson_4/index.js
@@ -0,0 +1,10 @@
+// if (1) {
+//   console.log("true");
+// }
+
+// //   if (1) {
+// //     console.log("false");
+// //   }
+// // }
+
+console.log("нееен");
---
+++ lesson_6/index.js
@@ -0,0 +1,54 @@
+// const obj1 = {
+//   name: "Alex",
+// };
+
+// const obj2 = obj1;
+
+// console.log(obj1, obj2);
+
+// obj1.name = "IVAN";
+
+// console.log(obj1, obj2);
+
+// const obj = {};
+
+// obj.true = 1;
+
+// console.log(obj);
+
+// const obj = {
+//   sayHi: function (name) {
+//     console.log("Hi User,", name);
+//   },
+// };
+
+// obj.sayHi("Nikita");
+
+// const myFavoriteFilm = {
+//   filmName: "Карты, деньги, 2 ствола",
+//   dateProduction: 1998,
+//   filmDirector: "Гай Ричи",
+//   countryProduction: "США",
+// };
+// console.log(myFavoriteFilm);
+
+// myFavoriteFilm.money = "1 000 000 долларов";
+
+// console.log(myFavoriteFilm);
+
+// delete myFavoriteFilm.dateProduction;
+
+// console.log(myFavoriteFilm);
+
+// const myFavoriteFilm = {
+//   filmName: function (name) {
+//     console.log("Название любимого фильма:", name);
+//   },
+// };
+
+// myFavoriteFilm.filmName("Карты, деньги, 2 ствола");
+
+// const str = "string";
+// console.log(str);
+// str[0 - "S"];
+// console.log(str);
